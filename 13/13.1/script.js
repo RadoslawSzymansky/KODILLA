18bc8c1fd7ddf5558ce10809595343e15f8260fb
Radoslaw Szymanski <radek199@gmail.com>
2019-03-26 23:35:09 +0100

    13.1 to check

--- 13/13.1/script.js
+++ 13/13.1/script.js
@@ -1,49 +1,54 @@
-(function () {
-    'use strict'
-    var data = [{
-            id: 'box1',
-            title: 'First box',
-            content: '<p>Lorem ipsum dolor sit amet!</p>',
-            categories: ['highlighted', 'special-header', 'important']
+(function() {
+    "use strict";
+    var data = [
+        {
+            id: "box1",
+            title: "First box",
+            content: "<p>Lorem ipsum dolor sit amet!</p>",
+            categories: ["highlighted", "special-header", "important"]
         },
         {
-            id: 'box2',
-            title: 'Second box',
-            content: '<p>Lorem ipsum dolor sit amet!</p>',
-            categories: ['special-header', 'important']
+            id: "box2",
+            title: "Second box",
+            content: "<p>Lorem ipsum dolor sit amet!</p>",
+            categories: ["special-header", "important"]
         },
         {
-            id: 'box3',
-            title: 'Third box',
-            content: '<p>Lorem ipsum dolor sit amet!</p>',
-            categories: ['highlighted', 'important']
+            id: "box3",
+            title: "Third box",
+            content: "<p>Lorem ipsum dolor sit amet!</p>",
+            categories: ["highlighted", "important"]
         },
         {
-            id: 'box4',
-            title: 'Fourth box',
-            content: '<p>Lorem ipsum dolor sit amet!</p>',
-            categories: ['highlighted']
+            id: "box4",
+            title: "Fourth box",
+            content: "<p>Lorem ipsum dolor sit amet!</p>",
+            categories: ["highlighted"]
         },
         {
-            id: 'box5',
-            title: 'Fifth box',
-            content: '<p>Lorem ipsum dolor sit amet!</p>',
+            id: "box5",
+            title: "Fifth box",
+            content: "<p>Lorem ipsum dolor sit amet!</p>",
             categories: []
-        },
+        }
     ];
 
     function createDivs(arr) {
         var fragment = document.createDocumentFragment();
-        arr.forEach(function (obj) {
-            var div = document.createElement('div');
-            div.setAttribute('id', obj.id);
-            div.innerHTML = `<header><h1>${obj.title}</h1></header>${obj.content}`;
-            if (obj.categories.length) obj.categories.forEach(e => div.classList.add(e));
+        arr.forEach(function(obj) {
+            var div = document.createElement("div");
+            div.setAttribute("id", obj.id);
+            var header = document.createElement("header");
+            var headerContent = document.createTextNode(obj.title);
+            header.appendChild(headerContent);
+            div.append(header);
+            div.append(obj.content);
+            if (obj.categories.length)
+                obj.categories.forEach(e => div.classList.add(e));
             fragment.appendChild(div);
-        })
-        document.body.appendChild(fragment)
+        });
+        return fragment;
     }
-    // events
-    document.querySelector('.start').addEventListener('click', createDivs.bind(null, data));
-
-})()
+    var readyDivs = createDivs(data);
+    document.body.append(readyDivs);
+})();
