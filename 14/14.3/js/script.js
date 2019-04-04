6d1e233b4bc4c0e7515ae7a80905fd122dfb773d
Radoslaw Szymanski <radek199@gmail.com>
2019-04-04 18:43:38 +0200

    14.4 to check

--- 14/14.3/js/script.js
+++ 14/14.3/js/script.js
@@ -23,5 +23,18 @@
         progress = Math.max(0, Math.min(1, progress));
         progressBar.style.width = progress * 100 + '%';
     });
+    window.initMap = function () {
+        var map = new google.maps.Map(
+            document.getElementById('map'), {
+                zoom: 4,
+                center: slideData[0].coords
+            });
+        slideData.forEach(e => {
+            new google.maps.Marker({
+                position: e.coords,
+                map: map
+            })
+        })
+    }
 
 })()
--- 14/14.4/js/script.js
+++ 14/14.4/js/script.js
@@ -24,4 +24,23 @@
         progressBar.style.width = progress * 100 + '%';
     });
 
+    window.initMap = function () {
+        var index = slideData.findIndex(e => e.id === window.location.hash.replace("#", ""))
+        index = index === -1 ? 0 : index;
+        var map = new google.maps.Map(
+            document.getElementById('map'), {
+                zoom: 6,
+                center: slideData[index].coords
+            });
+        slideData.forEach((e, i) => {
+            var marker = new google.maps.Marker({
+                position: e.coords,
+                map: map
+            })
+            marker.addListener('click', () => flkty.select(i));
+            flkty.on('change', function (index) {
+                if (event.target.tagName !== "AREA") map.panTo(slideData[index].coords);
+            });
+        })
+    }
 })()
