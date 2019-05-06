6309c20b35cee5d4762e67be2dc2dccded2d1b1e
Radoslaw Szymanski <radek199@gmail.com>
2019-05-06 16:57:35 +0200

    18.7 correction

--- 18/18.7/components/ContactForm.js
+++ 18/18.7/components/ContactForm.js
@@ -6,12 +6,11 @@ var ContactForm = React.createClass({
     render: function () {
         return (
                 <form className="contactForm">
-                <input placeholder="Imię" type="text" value={this.props.contact.firstName}/>
-                <input placeholder="Nazwisko" type="text" value={this.props.contact.lastName}/>
-                <input placeholder="Email" type="email" value={this.props.contact.email}/>
-                <button type="submit">Dodaj kontakt</button>
+                    <input placeholder="Imię" type="text" value={this.props.contact.firstName}/>
+                    <input placeholder="Nazwisko" type="text" value={this.props.contact.lastName}/>
+                    <input placeholder="Email" type="email" value={this.props.contact.email}/>
+                    <button type="submit">Dodaj kontakt</button>
                 </form>
-
         )
     },
 })
