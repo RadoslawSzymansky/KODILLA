0999ac9a79e52d48bb4b65e387765c4f13750064
Radoslaw Szymanski <radek199@gmail.com>
2019-05-15 20:38:34 +0200

    20 to check

---
+++ 20/20.7/src/containers/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './App.css';
+import Title from '../components/Title'
+import TodoList from '../components/TodoList'
+import { hot } from 'react-hot-loader';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [{
+                id: 1,
+                text: 'clean room'
+            }, {
+                id: 2,
+                text: 'wash the dishes'
+            }, {
+                id: 3,
+                text: 'feed my cat'
+            }]
+        };
+        this.addTodo = this.addTodo.bind(this);
+        this.removeTodo = this.removeTodo.bind(this);
+    }
+    addTodo(val) {
+        const todo = {
+            text: val,
+            id: uuid.v4(),
+        };
+        const data = [...this.state.data, todo];
+        this.setState({ data });
+    }
+    removeTodo(id) {
+        const remainder = this.state.data.filter(todo => todo.id !== id);
+        this.setState({ data: remainder });
+    }
+    render() {
+        const { data } = this.state;
+        return (
+            <div className={style.TodoApp}>
+                <Title taskNumber={data.length} />
+                <TodoList list={data} remove={this.removeTodo} />
+            </div>
+        );
+    }
+}
+
+export default hot(module)(App);
