8055d3d65e33f515909d1f3ce3db14a3eaefa9b6
Radoslaw Szymanski <radek199@gmail.com>
2019-07-06 17:22:12 +0200

    correction 27.4 27.7 mutating state

--- 27/27.4/comments.js
+++ 27/27.4/comments.js
@@ -19,25 +19,30 @@ function reducer(state = [], action) {
 
     case REMOVE_COMMENT:
       return state.comments.filter(comment => comment.id !== action.id);
-
+    
     case EDIT_COMMENT:
       return state.map(comment => {
-        if(comment.id ===  action.id) comment.text = action.text;
+        if (comment.id === action.id) {
+          return { ...comment, text: action.text }
+        }
         return comment;
       });
 
     case THUMB_DOWN_COMMENT:
-      return  state.map(comment => {
-          if (comment.id === action.id) comment.votes--;
-          return comment;
-      });
+      return state.map(comment => {
+        if (comment.id === action.id) {
+          return { ...comment, votes: comment.votes - 1 }
+        }
+        return comment;
+    });
 
     case THUMB_UP_COMMENT:
       return state.map(comment => {
-          if (comment.id === action.id) comment.votes++;
-          return comment;
-      });
-
+        if (comment.id === action.id) {
+          return { ...comment, votes: comment.votes + 1 }
+        }
+        return comment;
+    });
     default:
       return state;
   };
