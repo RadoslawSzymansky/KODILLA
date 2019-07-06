1c498f10d5b520e1c2de3bb6a91a0cfd016d0da0
Radoslaw Szymanski <radek199@gmail.com>
2019-07-06 17:04:15 +0200

    27.7 to check:

---
+++ 27/27.7/comment_app/src/comments.js
@@ -0,0 +1,46 @@
+import { 
+  REMOVE_COMMENT,
+  EDIT_COMMENT,
+  THUMB_UP_COMMENT,
+  THUMB_DOWN_COMMENT,
+  ADD_COMMENT
+} from './actions';
+
+
+function reducer(state = [], action) {
+  switch (action.type) {
+
+    case ADD_COMMENT:
+      return [...state, {
+        id: action.id,
+        text: action.text,
+        votes: 0
+      }];
+
+    case REMOVE_COMMENT:
+      return state.comments.filter(comment => comment.id !== action.id);
+
+    case EDIT_COMMENT:
+      return state.map(comment => {
+        if(comment.id ===  action.id) comment.text = action.text;
+        return comment;
+      });
+
+    case THUMB_DOWN_COMMENT:
+      return  state.map(comment => {
+          if (comment.id === action.id) comment.votes--;
+          return comment;
+      });
+
+    case THUMB_UP_COMMENT:
+      return state.map(comment => {
+          if (comment.id === action.id) comment.votes++;
+          return comment;
+      });
+
+    default:
+      return state;
+  };
+};
+
+export default reducer;
